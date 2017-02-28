40a5669b04a48f40c77f9adec0052c8370f49a81
AIM <abzalov.im@gmail.com>
2017-02-28 08:07:18 +0300

    init

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+#define MAX_PLANET 15
+
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
@@ -14,10 +16,18 @@ public:
     int boss;
     int gen;
     int cur;
+	int base_gen=-1;
+	int freeze=-1;
+	int marker;
     void init(int b, int c, int g){
         boss=b;
         gen =g;
         cur =c;
+		base_gen=max(gen,base_gen);
+		if(gen<base_gen)
+			if(freeze<0)freeze=5;
+			else freeze--;
+		else freeze=-1;
     }
 };
  
@@ -36,6 +46,123 @@ public:
         rem =r;
     }
 };
+
+class User{
+public:
+	int bombs;
+	vector<Planet> pl;
+	vector<Troop> tr;
+	User(){
+		bombs=2;
+	}
+	void init(){
+		pl.clear();
+		tr.clear();
+	}
+	void add_pl(int c, int g){
+		
+	}
+};
+
+class Solution{
+private:
+	int state; //0-init, 1-debut, 2-mid, 3-end
+	int plan;
+	int pl_id, m_cnt, e_cnt, n_cnt;
+	int factoryCount;
+	int ds[MAX_PLANET][MAX_PLANET];
+	Planet all_p[MAX_PLANET];
+	vector<int> my_p,en_p,ne_p;
+public:
+	Solution(int fC){
+		state=0;
+		plan =0;
+		factoryCount = fC;
+		for(int i=0;i<factoryCount;i++)
+			for(int j=0;j<factoryCount;j++)
+			{
+				ds[i][j]=0;
+				ds[j][i]=0;
+			}
+	}
+	void add_dist(int f, int t, int d){
+		ds[f][t]=d;
+		ds[t][f]=d;
+	} 
+	void add_planet(int b, int c, int g){
+		all_p[pl_id].init(b,c,g);
+		if(b==1)my_p.push_back(pl_id);
+		else if(b==-1)en_p.push_back(pl_id);
+		else ne_p.push_back(pl_id);
+		pl_id++;
+	}
+	void add_troop(int b, int f, int t, int c, int r){
+		
+	}
+	void add_bomb(int b, int f, int t, int r){
+		
+	}
+	void init(){
+		pl_id=0;
+		m_cnt=0;
+		e_cnt=0;
+		n_cnt=0;
+	}
+	//------------------------------------------logic------------------------------------------
+	void create_markers(){
+		for(auto i : my_p){
+			for(int j=0;j<factoryCount;j++){
+				int d1,d2;
+				d1=ds[*i][j];
+				d2=9999;
+				for(auto k : en_p)
+					d2=min(d2,ds[*k][j]);
+				if(d1<d2-3)all_p[j].marker=0;// my zone
+				else if(d2<d1-3)all_p[j].marker=10; //enemy
+				else if(d1<d2)
+					if((all_p[j].base_gen>1)&&(all_p[j].cur>6))all_p[j].marker=1;//my opt
+					else all_p[j].marker=2;//my nw
+				else if(d2>d1)
+					if((all_p[j].base_gen>1)&&(all_p[j].cur>6))all_p[j].marker=11;//en opt
+					else all_p[j].marker=12;//en nw
+				else all_p[j].marker=100;
+			}
+		}
+	}
+	void get_plan(){
+		create_markers();
+		if()
+	}
+	void debut(){
+		
+	}
+	void defence(){
+		
+	}
+	void get_final(){
+		
+	}
+	void out(){
+		cout<<"WAIT"<<endl;
+	}
+	void calc(){
+		switch(state){
+			case 0 : 
+				get_plan();
+				break;
+			case 1 : 
+				debut();
+				break;
+			case 2 : 
+				defence();
+				break;
+			case 3 : 
+				get_final();
+				break;
+		}
+		out();
+	}
+};
  
 int main()
 {
@@ -43,33 +170,22 @@ int main()
     cin >> factoryCount; cin.ignore();
     int linkCount; // the number of links between factories
     cin >> linkCount; cin.ignore();
-    int ds[factoryCount][factoryCount];
-    Planet pl[factoryCount];
-    for(int i=0;i<factoryCount;i++)
-        for(int j=0;j<factoryCount;j++)
-        {
-            ds[i][j]=0;
-            ds[j][i]=0;
-        }
+	Solution sl(factoryCount);
     for (int i = 0; i < linkCount; i++) {
         int factory1;
         int factory2;
         int distance;
         cin >> factory1 >> factory2 >> distance; cin.ignore();
-        ds[factory1][factory2]=distance;
-        ds[factory2][factory1]=distance;
+		sl.add_dist(factory1,factory2,distance);
     }
 
     // game loop
-    int cfree=5;
     while (1) {
+		sl.init();
         int entityCount; // the number of entities (e.g. factories and troops)
         
         cin >> entityCount; cin.ignore();
         int id=0;
-        Troop tr[entityCount-factoryCount];
-        vector<int> my;
-        int td=0;
         for (int i = 0; i < entityCount; i++) {
             int entityId;
             string entityType;
@@ -81,74 +197,14 @@ int main()
             cin >> entityId >> entityType >> arg1 >> arg2 >> arg3 >> arg4 >> arg5; cin.ignore();
             //cerr<< entityId << entityType << arg1 << arg2 << arg3 << arg4 << arg5<<endl;
             if(entityType=="FACTORY"){
-                pl[id++].init(arg1, arg2, arg3);
-                if(arg1==1) my.push_back(id-1);
+                sl.add_planet(arg1, arg2, arg3);
             } else {
-                if(entityType=="BOMB") cerr<<entityType<<" "<<arg1 << " "<<arg2<<" " << arg3 <<" "<< arg4<<" " << arg5<<endl;
-                tr[td++].init(arg1, arg2, arg3, arg4, arg5);
+                if(entityType=="BOMB")
+					sl.add_bomb(arg1, arg2, arg3, arg4);
+				else
+					sl.add_troop(arg1, arg2, arg3, arg4, arg5);
             }
         }
-        int mn=999,mt=-1,mf,snd;
-        int cr,ma=0;
-        for(auto it : my){
-            for(int i=0;i<factoryCount;i++){
-                if(pl[i].boss!=1){
-                    cr=pl[i].cur;
-                    if(pl[i].boss!=0) cr+=(pl[i].gen*(ds[it][i]+1));
-                    if((ds[it][i]-(pl[i].gen*2)<mn)&&(pl[it].cur>cr)&&(pl[i].gen>0)){
-                        mn=ds[it][i]-(pl[i].gen*2);
-                        mf=it;
-                        mt=i;
-                        snd=pl[mf].cur;
-                    }
-                }
-            }
-        }
-        if(mt>=0){
-            cfree=5;
-            cout<<"MOVE "<<mf<<" "<<mt<<" "<<snd<<endl;
-        }else{
-            int ap=1;
-            ma=0;
-            for(int it=0; it<factoryCount; it++){
-                if(pl[it].boss==-1){
-                    if(pl[it].gen>0)ap=0;
-                    ma=1;
-                    break;
-                }
-            }
-            if((ma==1)&&(ap==1)){
-                
-                mn=999;
-                mt=-1;
-                for(auto it : my){
-                    if(pl[it].gen>0)
-                        for(int i=0;i<factoryCount;i++){
-                            if(pl[i].boss==-1){
-                                cr=pl[i].cur+(pl[i].gen*(ds[it][i]+1));
-                                if((ds[it][i]<mn)&&(pl[it].cur>cr*2)){
-                                    mn=ds[it][i]-(pl[i].gen*2);
-                                    mf=it;
-                                    mt=i;
-                                    snd=pl[it].cur/2+1;
-                                }
-                            }
-                        }
-                }
-            }
-            else cfree=5;
-            if(mt>=0){
-                cfree--;
-                if(cfree==0){
-                    cout<<"MOVE "<<mf<<" "<<mt<<" "<<snd<<endl;
-                    cfree=5;
-                }else{
-                    cout << "WAIT" << endl;
-                }
-            }else{
-                cout << "WAIT" << endl;
-            }
-        }
-
+		sl.calc();
     }
 }
